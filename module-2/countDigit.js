625a1cb3b88e6ee001cc6fcfdce008328c065797
Akash Karwande <akash.karwande@oracle.com>
2025-09-24 18:29:06 +0530

    count number of digit

---
+++ module-2/countDigit.js
@@ -0,0 +1,16 @@
+function countNumberOfDigits(n) {
+    if(n == 0) return 1;
+
+    // convert negative number to positive
+    n = Math.abs(n);
+    let count = 0;
+    while(n > 0) {
+        n = Math.floor(n/10);
+        count++
+    }
+
+    return count
+
+}
+
+console.log(countNumberOfDigits(-9123))
