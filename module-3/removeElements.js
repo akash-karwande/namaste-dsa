f75341b512767c94624c09e5bb55b3ab854539c5
Akash Karwande <akash.karwande@oracle.com>
2025-09-29 11:29:13 +0530

    remove element

---
+++ module-3/removeElements.js
@@ -0,0 +1,16 @@
+// Input: nums = [3,2,2,3], val = 3
+// Output: 2, nums = [2,2,_,_]
+
+
+function removeElements(arr, ele) {
+    let x = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] !== ele) {
+            arr[x] = arr[i];
+            x = x +1
+        }
+    }
+    console.log(arr)
+}
+
+console.log(removeElements([3,2,2,3], 3));
