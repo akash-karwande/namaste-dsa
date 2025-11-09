430a236f1f9723c4a50f0f48c43906adf7fcf708
Akash Karwande <akashnkarwande@gmail.com>
2025-11-09 22:23:32 +0530

    single digit count element from array

---
+++ module-3/singleOne.js
@@ -0,0 +1,24 @@
+// input arr = [2,2,1]
+// output 1
+
+// input arr = [1,2,3,4, 1,2,3]
+// output 4
+
+// other numbers appears two times but 1 and 4 only times
+
+function findSingleCountDigit(arr) {
+    let xor = 0;
+    for(let i=0; i< arr.length; i++) {
+        xor = xor ^ arr[i]
+    }
+    return xor
+}
+
+console.log(findSingleCountDigit([2,2,1]));
+
+// ^ bitwise xor operator
+
+// a ^ 0 = a;
+// a ^ a = 0
+
+// so bitwise xor(^) operator can be used to remove duplicates 
